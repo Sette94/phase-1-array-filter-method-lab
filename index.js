eabffa92d118a5d17fb1d142658aee6cdceb1c89
Nick Sette <njs2394@gmail.com>
2023-11-01 20:44:03 -0400

    filtering

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+
+
+function findMatching(arr, string) {
+    let drivers = arr.filter(el => string.toLowerCase() == el.toLowerCase())
+    return drivers
+
+}
+
+
+const workers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+const startingLet = "Bo"
+fuzzyMatch(workers, startingLet)
+
+function fuzzyMatch(arr, string) {
+    let drivers = arr.filter(el => el.startsWith(string))
+    debugger
+    return drivers
+
+}
+
+
+
+function matchName(arr, string) {
+    let drivers = arr.filter(el => el.name == string)
+    debugger
+    return drivers
+
+
+}
